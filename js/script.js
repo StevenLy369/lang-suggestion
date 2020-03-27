d51edfe8fc342fd7e8b6d9b416fd0f3378234b1d
Steven Ly <1stevenly92@gmail.com>
2020-03-27 11:33:18 -0700

    added some form validaton where if nothing is selected and is subbmited a popup will show

--- js/script.js
+++ js/script.js
@@ -4,22 +4,43 @@ $(document).ready(function(){
         var animal = $("#animal").val();
         var food = $("#food").val();
         var programming = $("input:radio[name=programming]:checked").val();
+        var romantic =$("input:radio[name=romantic]:checked").val();
 
 
         if (programming == "no"){
+            $("#formOne").fadeOut();
             $("#nothing").show();
+            
 
             
-         } else {
-             $("#javascript").show();
-             console.log("javascript");
-         }
+         } else if (animal == "Snakes") {
+             $("#python").show();
+             $("#formOne").fadeOut();
+             
+            
+         }else if ((animal == "Llama" || romantic == "yes")  && (food == "Taco Bell" || food == "Pizza") ){
+            $("#formOne").fadeOut();
+            $("#javascript").show();
+
+        }else if ((animal == "Cats" || romantic == "no") && (food == "McDonalds" || food =="Taco Bell" )){
+            $("#formOne").fadeOut();
+            $("#c").show();
+
+        }else if ((programming == undefined)) {
+            alert("Please refresh and select all options");
+    
+
+            
+        }else {
+            $("#swift").show();
+            $("#formOne").fadeOut();
+        }
     
   
   
-        
+        console.log(programming);
 
-    $("#formOne").hide();
+    // $("#formOne").fadeOut();
     event.preventDefault();
     
     
