0a825975b53769ce5b94655ec397573757f3a28b
Steven Ly <1stevenly92@gmail.com>
2020-03-27 08:48:24 -0700

    added very basic if else statement to check if everything is working

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@ $(document).ready(function(){
     $("#formOne").submit(function(event) {
         var name = $("#name").val();
         var animal = $("#animal").val();
+        var food = $("#food").val();
+        var programming = $("input:radio[name=programming]:checked").val();
+
+
+        if (programming == "no"){
+            console.log("cat");
+
+            
+         } else {
+             console.log("dog")
+         }
     
   
   
-
+        console.log(name);
+        console.log(animal);
+        console.log(food);
+        
 
     $("#formOne").hide();
     event.preventDefault();
-    console.log(name);
-    console.log(animal);
+    
     
     });
   });
