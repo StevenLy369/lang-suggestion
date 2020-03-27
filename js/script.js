a28e6f4d64af6a7f2c3f38b5bca7ef537a6f69bb
Steven Ly <1stevenly92@gmail.com>
2020-03-27 12:18:18 -0700

    updated readme and uploaded pics to html page

--- js/script.js
+++ js/script.js
@@ -5,40 +5,42 @@ $(document).ready(function(){
         var food = $("#food").val();
         var programming = $("input:radio[name=programming]:checked").val();
         var romantic =$("input:radio[name=romantic]:checked").val();
+        // var results = $(name + ", You should try out:")
 
 
-        if (programming == "no"){
+        if (programming === "no"){
             $("#formOne").fadeOut();
             $("#nothing").show();
             
 
-            
-         } else if (animal == "Snakes") {
-             $("#python").show();
-             $("#formOne").fadeOut();
-             
-            
-         }else if ((animal == "Llama" || romantic == "yes")  && (food == "Taco Bell" || food == "Pizza") ){
+        }else if ((programming === undefined || romantic === undefined || name == "")) {
+            alert("Please select all options");
+
+       } else if (animal == "Snakes") {
+            $("#python").show();
             $("#formOne").fadeOut();
+            var results = (name + ", You should try out:")
+            $(".playerName").text(results);
+        
+            
+        }else if ((animal == "Llama" && romantic == "yes")  && (food == "Taco Bell" || food == "Pizza") ){
             $("#javascript").show();
-
-        }else if ((animal == "Cats" || romantic == "no") && (food == "McDonalds" || food =="Taco Bell" )){
             $("#formOne").fadeOut();
+            var results = (name + ", You should try out:")
+            $(".playerName").text(results);
+
+        }else if ((animal == "Cats" && romantic == "no") && (food == "McDonalds" || food =="Taco Bell" )){
             $("#c").show();
+            $("#formOne").fadeOut();
+            var results = (name + ", You should try out:")
+            $(".playerName").text(results);
 
-        }else if ((programming == undefined)) {
-            alert("Please refresh and select all options");
-    
-
-            
         }else {
             $("#swift").show();
             $("#formOne").fadeOut();
+            var results = (name + ", You should try out:")
+            $(".playerName").text(results);
         }
-    
-  
-  
-        console.log(programming);
 
     // $("#formOne").fadeOut();
     event.preventDefault();
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Language Suggester
+The Purpose of this program is to find out what language best fits you.
+
+## Installation
+Clone the project, open in a editor of your choice and open live server.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
